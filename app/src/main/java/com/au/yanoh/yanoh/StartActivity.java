db58538e0271183eda177906578cc69650074737
ahmeturganci <aahmet.272@gmail.com>
2017-06-19 13:36:19 +0300

    Giriş ekranı animasyon eklendi.

--- app/src/main/java/com/au/yanoh/yanoh/StartActivity.java
+++ app/src/main/java/com/au/yanoh/yanoh/StartActivity.java
@@ -1,13 +1,33 @@
 package com.au.yanoh.yanoh;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class StartActivity extends AppCompatActivity {
+    private Handler mHandler = new Handler();
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        view = findViewById(android.R.id.content);
+        Animation mLoadAnimation = AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_in);
+        mLoadAnimation.setDuration(2000);
+        view.startAnimation(mLoadAnimation);
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        },4000);
     }
 }
